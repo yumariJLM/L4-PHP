78d51b8456d5e54ecdad04bb5f2ee44845b332db
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-01 19:41:23 -0600

    Ejercicio1

--- index.php
+++ index.php
@@ -4,7 +4,13 @@
 
 <h1>Ejercicios PHP</h1>
 <ul class="pagination">
+  <li><a href="Ejercicio1.php" target ="new"> Ejercicio1</a></li>
   <li><a href="#" target ="new"> Ejercicio2</a></li>
+  <li><a href="#" target ="new"> Ejercicio3</a></li>
+  <li><a href="#" target ="new"> Ejercicio4</a></li>
+  <li><a href="#" target ="new"> Ejercicio5</a></li>
+  <li><a href="#" target ="new"> Ejercicio6</a></li>
+
 
 </ul>
 
