61f49962e122638be3ecdf5422272b74b15637fa
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-02 16:34:57 -0600

    Update Ejercicio.php

--- Ejercicio.php
+++ Ejercicio.php
@@ -4,25 +4,20 @@
 
 <?php
 
-$CampoTexto = $_GET['Campo1'];
-echo $CampoTexto;
-
-if ($CampoTexto % 2 != 0) {
-    echo "Es impar!";
-  } else {
-    echo "Es par";
-  }
-
+$num = $_GET['Campo1'];
+$i=2;
+$primo= true;
+while ($primo && $i<$num){
+    $primo= ($num %$i)!=0;
+    $i++;
+}
+if ($primo){
+   echo "el numero $num es primo"; 
+}else{
+    echo "el numero $num no es primo";  
+}
 
 ?>
-
-
-<form action="Ejercicio.php" method="get">
- <input type="number" id="campo1" name="campo1" placeholders="Inserta un numero"/>
- <input type="submit" value="Enviar"/>
-</form>
-
-
  
 </body>
 </html>
