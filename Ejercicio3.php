161e7c05526599e6d26e9e58b54f2e6c238a4169
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-01 21:13:24 -0600

    Ejercicio 3

---
+++ Ejercicio3.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Ejercicio 3</h1>
+<?php
+
+
+$miarreglo = array (10,20,30,40,50);
+
+echo "<p>Contenido del elemento 2 del arreglo: $miarreglo [2]</p>";
+
+echo "<p>Despliega el contenido del arreglo: </p>";
+print_r ($miarreglo);
+
+
+echo "<p>El tamaño del arreglo es:" .sizeof($miarreglo)."</p>";
+
+$miFechaArr ["dia"]= "Lunes";
+$miFechaArr ["mes"]= "Mayo";
+$miFechaArr ["año"]= "2020";
+
+echo "<br><br>";
+
+var_dump ($miFechaArr);
+unset ($miFechaArr["año"]);
+echo "<br><br>";
+print_r ($miFechaArr);
+$miarregloReves = array_reverse($miarreglo);
+
+echo "<br><br>";
+print_r($miarregloReves);
+
+
+
+
+
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <ul class="pagination">
   <li><a href="Ejercicio1.php" target ="new"> Ejercicio1</a></li>
   <li><a href="Ejercicio2.php" target ="new"> Ejercicio2</a></li>
-  <li><a href="#" target ="new"> Ejercicio3</a></li>
+  <li><a href="Ejercicio3.php" target ="new"> Ejercicio3</a></li>
   <li><a href="#" target ="new"> Ejercicio4</a></li>
   <li><a href="#" target ="new"> Ejercicio5</a></li>
   <li><a href="#" target ="new"> Ejercicio6</a></li>
