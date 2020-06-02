f3c084979a7d2a4f30e2433b1cfbd5a55a2e6765
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-01 22:58:01 -0600

    Update Ejercicio7.php

--- Ejercicio7.php
+++ Ejercicio7.php
@@ -8,13 +8,7 @@
 
 header ("Content-type: text/html;charset=\"utf-8\"");
 
-for ($i=1; $i<=100; $i++) {
-    if (primo($i)) {
-        echo "<br>El número ".$i." es primo";
-    } else {
-        echo "<br>El número ".$i." NO es primo";
-    }
-}
+
  
 $num= 14;
 function primo($num)
@@ -22,15 +16,17 @@ function primo($num)
     if ($num == 2 || $num == 3 || $num == 5 || $num == 7) {
         return True;
     } else {
-        // comprobamos si es par
+        
         if ($num % 2 != 0) {
-            // comprobamos solo por los impares
+        
             for ($i = 3; $i <= sqrt($num); $i += 2) {
                 if ($num % $i == 0) {
                     return False;
+                    echo "<br>El número ".$num." NO es primo";
                 }
             }
-            return True;
+            return True;echo
+             "<br>El número ".$num." es primo";
         }
     }
     return False;
