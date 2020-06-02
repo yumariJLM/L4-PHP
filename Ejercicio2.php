e086dd915611d0685c62a227a760f5bbff1487f6
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-01 20:16:03 -0600

    Ejercicio 2

---
+++ Ejercicio2.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Ejercicio 2</h1>
+
+<?php
+//Este es un comentario de una linea
+
+/*
+Este es
+
+ un comentario de multiples lineas
+ linea
+*/
+
+$NombreClase= "Lenguaje 4";
+echo "<html><head><meta charset= \"utf-8\"></head>";
+echo "<body>";
+echo "<h2>Esta es la clase de $NombreClase mañana y tarde (Simbolo) </h2>";
+echo "<h2>Esta es otra fila de la clase de "$.NombreClase. "con dos cadenas concatenadas</h2>";
+echo "</body>";
+echo "</html>";
+
+$numero = 50;
+$calculo = $numero/2+6;
+echo "El resultado de la operacion es: $calculo";
+
+$logico = true;
+$logico2= false;
+echo "<p>El contenido de una variable tipo booleano verdadero: $logico y variable booleana falsa: $logico2 </p>";
+$variableNombreClase = "NombreClase";
+echo "<p>Esta es un nombre de variable extraido de una cadena".$$variableNombreClase." </p>";
+
+
+
+?>
+
+</body>
+</html>
