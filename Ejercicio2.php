eca4162c8c9e76de535c7c227c454670fbf61e76
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-01 20:27:33 -0600

    Update Ejercicio2.php

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -13,23 +13,24 @@ Este es
  linea
 */
 
-$NombreClase= "Lenguaje 4";
-echo "<html><head><meta charset= \"utf-8\"></head>";
+$NombreClase = "Lenguaje 4";
+echo "<html><head><meta charset=\"utf-8\"></head>";
 echo "<body>";
 echo "<h2>Esta es la clase de $NombreClase mañana y tarde (Simbolo) </h2>";
-echo "<h2>Esta es otra fila de la clase de "$.NombreClase. "con dos cadenas concatenadas</h2>";
+echo "<h2>Esta es otra fila de la clase de " .$NombreClase. " con dos cadenas concatenadas</h2>";
 echo "</body>";
 echo "</html>";
 
 $numero = 50;
-$calculo = $numero/2+6;
+$calculo = $numero / 2 + 6;
 echo "El resultado de la operacion es: $calculo";
 
 $logico = true;
 $logico2= false;
-echo "<p>El contenido de una variable tipo booleano verdadero: $logico y variable booleana falsa: $logico2 </p>";
+echo "<p>El contenido de una variable tipo booleano verdadero: $logico y variable booleana falsa: $logico2</p>";
+
 $variableNombreClase = "NombreClase";
-echo "<p>Esta es un nombre de variable extraido de una cadena".$$variableNombreClase." </p>";
+echo "<p>Esta es un nombre de variable extraido de una cadena".$$variableNombreClase."</p>";
 ?>
 
 </body>
