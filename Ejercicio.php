efd1a07e369b605e40368134075eca5c56fc6259
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-02 18:24:12 -0600

    Update Ejercicio.php

--- Ejercicio.php
+++ Ejercicio.php
@@ -6,31 +6,21 @@
 
 $num = $_GET['Campo1'];
 
-for ($i=1; $i<=100; $i++) {
-    if (primo($i)) {
-        echo "<br>El número ".$i." es primo";
-    } else {
-        echo "<br>El número ".$i." NO es primo";
-    }
- 
-
-function primo($num)
-{
-    if ($num == 2 || $num == 3 || $num == 5 || $num == 7) {
-        return True;
-    } else {
-        // comprobamos si es par
-        if ($num % 2 != 0) {
-            // comprobamos solo por los impares
-            for ($i = 3; $i <= sqrt($num); $i += 2) {
-                if ($num % $i == 0) {
-                    return False;
-                }
+if ($num == 2 || $num == 3 || $num == 5 || $num == 7) {
+    echo "Es un numero primo";
+} else {
+    // comprobamos si es par
+    if ($num % 2 != 0) {
+        // comprobamos solo por los impares
+        for ($i = 3; $i <= sqrt($num); $i += 2) {
+            if ($num % $i == 0) {
+                echo "No es un numero primo";
             }
-            return True;
         }
+        echo "Es un numero primo";
     }
-    return False;
+}
+echo "No es un numero primo";
 }
 
 ?>
