7cca38f19c70fed6ab42d870869a3368dfccc2d1
yumariJLM <51842181+yumariJLM@users.noreply.github.com>
2020-06-02 19:29:37 -0600

    Ejercicio 7 terminado

--- Ejercicio.php
+++ Ejercicio.php
@@ -6,23 +6,17 @@
 
 $num = $_GET['Campo1'];
 
-if ($num == 2 || $num == 3 || $num == 5 || $num == 7) {
-    echo "Es un numero primo";
+if ($num == 2 || $num == 3 || $num == 5 || $num == 7 ) 
+{
+  echo $num."  : es un numero primo";
+  
+} elseif ($num % 2 != 0 && $num % 3 !=0 && $num % 5 !=0 && $num % 7 !=0)
+{
+  echo $num."  : es un numero primo";
+  
 } else {
-    // comprobamos si es par
-    if ($num % 2 != 0) {
-        // comprobamos solo por los impares
-        for ($i = 3; $i <= sqrt($num); $i += 2) {
-            if ($num % $i == 0) {
-                echo "No es un numero primo";
-            }
-        }
-        echo "Es un numero primo";
-    }
+  echo $num."  : no es un numero primo";
 }
-echo "No es un numero primo";
-}
-
 ?>
 
 
